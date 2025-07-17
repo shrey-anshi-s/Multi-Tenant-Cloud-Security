b741b43b3ddcfc0cdaf4a3c1ceea8497679e963b
shrey-anshi-s <120453595+shrey-anshi-s@users.noreply.github.com>
2025-07-17 13:20:07 +0530

    Update create_vms.sh

--- vm_scripts/create_vms.sh
+++ vm_scripts/create_vms.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+TENANT=$1
+if [ -z "$TENANT" ]; then
+  echo "Usage: ./create_tenants.sh tenant-name"
+  exit 1
+fi
+
+IMAGE_NAME="${TENANT}-image"
+CONTAINER_NAME="$TENANT"
+HOSTNAME="$TENANT-host"
+VOLUME_PATH="/mnt/${TENANT}-secure"
+
+echo "🚀 Creating container: $CONTAINER_NAME"
+docker run -dit \
+  --name "$CONTAINER_NAME" \
+  --hostname "$HOSTNAME" \
+  -v "$VOLUME_PATH:/mnt/secure" \
+  --cap-add=SYS_ADMIN \
+  --cap-add=NET_ADMIN \
+  "$IMAGE_NAME"
